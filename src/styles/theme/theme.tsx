e5d18b7a6dcdd7e50efdb281b9d293182e9f4fdc
EstebanBedoya <estebegi@gmail.com>
2024-01-10 22:12:46 -0500

    style: mui theme

--- src/styles/theme/theme.tsx
+++ src/styles/theme/theme.tsx
@@ -1,29 +1,46 @@
-import { Roboto } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
+/** @package */
+import { Roboto } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface PaletteOptions {
+    violet?: PaletteOptions["primary"];
+  }
+}
 
 const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const theme = createTheme({
   palette: {
-    mode: 'light',
+    mode: "light",
+    primary: {
+      main: "#0054A3",
+    },
+    success: {
+      main: "#00AA07",
+    },
+    error: {
+      main: "#E40000",
+      light: "#E4000080",
+    },
+    warning: {
+      main: "#FFA800",
+      light: "#FFA80080",
+    },
+    violet: {
+      main: "#9431D0",
+    },
+    text:{
+      primary: "#000000",
+      secondary: "#00000080",
+    }
   },
   typography: {
-    fontFamily: roboto.style.fontFamily,
-  },
-  components: {
-    MuiAlert: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          ...(ownerState.severity === 'info' && {
-            backgroundColor: '#60a5fa',
-          }),
-        }),
-      },
-    },
+    fontFamily: ['sans-serif'].join(','),
   },
 });
 
