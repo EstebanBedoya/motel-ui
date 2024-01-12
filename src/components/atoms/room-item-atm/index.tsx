e86890a40356fedf0a54de5e33a1cda7b8db676b
EstebanBedoya <estebegi@gmail.com>
2024-01-12 01:40:09 -0500

    feat: filter tabs funcionality

--- src/components/atoms/room-item-atm/index.tsx
+++ src/components/atoms/room-item-atm/index.tsx
@@ -2,7 +2,43 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
-const RoomItemAtm = ({ color = "#00AA07" }: { color?: string }) => {
+/** @style */
+import { useTheme } from "@mui/material";
+
+enum RoomStates {
+  AVAILABLE = "available",
+  OCCUPIED = "occupied",
+  MAINTENANCE = "maintenance",
+  CLEANING = "cleaning",
+}
+
+interface Props {
+  roomId: number;
+  state: RoomStates;
+  type: string;
+}
+
+const RoomStatesSpanish = {
+  [RoomStates.AVAILABLE]: "Disponible",
+  [RoomStates.OCCUPIED]: "Ocupada",
+  [RoomStates.MAINTENANCE]: "Mantenimiento",
+  [RoomStates.CLEANING]: "Limpieza",
+};
+
+const RoomItemAtm = ({ 
+  roomId, 
+  state,
+  type 
+}: Props) => {
+  const theme = useTheme();
+
+  const color = {
+    [RoomStates.AVAILABLE]: theme.palette.success.main,
+    [RoomStates.OCCUPIED]: theme.palette.error.main,
+    [RoomStates.MAINTENANCE]: theme.palette.warning.main,
+    [RoomStates.CLEANING]: theme.palette.violet.main,
+  }[state];
+
   return (
     <Box
       alignItems="center"
@@ -30,15 +66,15 @@ const RoomItemAtm = ({ color = "#00AA07" }: { color?: string }) => {
         width={90}
       >
         <Typography fontSize={40} fontWeight={700} color={color}>
-          01
+          {roomId}
         </Typography>
       </Box>
       <Box display="flex" flexDirection="column" alignItems="center">
         <Typography fontSize={25} fontWeight={600}>
-          SENCILLA
+          {type}
         </Typography>
         <Typography fontSize={20} fontWeight={600} color={color}>
-          Disponible
+          {RoomStatesSpanish[state]}
         </Typography>
       </Box>
     </Box>
